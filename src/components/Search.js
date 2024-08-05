582734a1dd24eedb668418405fd89c288f5f631f
shanavassharu <shanavassharu91@gmail.com>
2024-08-05 12:55:12 +0530

    update all files

---
+++ src/components/Search.js
@@ -0,0 +1,9 @@
+//search Component
+function Search() {
+    return (
+        <div className="searchbar">
+            <input type="text" placeholder="Search" />
+        </div>
+    )
+}
+export default Search
